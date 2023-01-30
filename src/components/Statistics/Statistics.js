b09bea3742cf09fa537fce922a6805876cb52338
AniaPiWo <snowflake@wp.pl>
2023-01-30 12:51:46 +0100

    fixed

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,25 +1,32 @@
+import css from "./Statistics.module.css"
 import PropTypes from 'prop-types';
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 export const Statistics = ({ title, stats }) => {
 
     return (
 
-      <section>
+      < section className={css.statistics} >
 
-        {title && <h2 className={title}>{title}</h2>}
+        {title && <h2 className={css.title}>{title}</h2>}
   
-        <ul className={statList}>
+        <ul className={css.statList}>
           {stats.map(stat => (
-            <li
+            <li 
+              style={{backgroundColor: getRandomHexColor()}}
               key={stat.id}
-              className={item}
+              className={css.item}
             >
-              <span className={labek}>{stat.label}</span>
-              <span className={percentage}>{stat.percentage}%</span>
+              <span className={css.label}>{stat.label}</span>
+              <span className={css.percentage}>{stat.percentage}%</span>
             </li>
           ))}
         </ul>
-        
+
       </section>
     );
   };
