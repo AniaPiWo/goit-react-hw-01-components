4398ac8116f834099ae94ad1906572c305af3064
AniaPiWo <snowflake@wp.pl>
2023-01-31 10:49:06 +0100

    prettier

--- src/components/Transactions/TransactionHistory.js
+++ src/components/Transactions/TransactionHistory.js
@@ -2,43 +2,37 @@ import css from './Transaction.module.css';
 import PropTypes from 'prop-types';
 
 export const TransactionHistory = ({ items }) => {
-    return (
-      <section className={css.transactionHistory}>
-  
-        <table className={css.transactionHistoryTable}>
-            
-            <thead>
-                <tr className={css.tableRowHead}>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th>Currency</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                {items.map(item => (
-                    <tr 
-                    key={item.id}
-                    className={css.tableRowBody}>
-                        <td>{item.type}</td>
-                        <td>{item.amount}</td>
-                        <td>{item.currency}</td>
-                    </tr>
-            ))}
-            </tbody>
-        </table>
-        
-      </section>
-    )
-}
+  return (
+    <section className={css.transactionHistory}>
+      <table className={css.transactionHistoryTable}>
+        <thead>
+          <tr className={css.tableRowHead}>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(item => (
+            <tr key={item.id} className={css.tableRowBody}>
+              <td>{item.type}</td>
+              <td>{item.amount}</td>
+              <td>{item.currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+};
 
 TransactionHistory.propTypes = {
-    items: PropTypes.arrayOf(
-      PropTypes.exact({
-        id: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired,
-        amount: PropTypes.string.isRequired,
-        currency: PropTypes.string.isRequired,
-      })
-    ).isRequired,
-  };
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
